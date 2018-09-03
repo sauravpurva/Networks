bc422f8694584b1959e2f4d571691f1eb0f33283
sauravpurva <ced16i029@iiitdm.ac.in>
2018-09-03 18:37:39 +0530

    second

---
+++ framing/character_count.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string s;
+	cout<<"Enter Data Frame:\n";
+	cin>>s;
+	cout<<"Frame sent by sender:\n";
+	cout<<s.length()<<s<<endl;
+}
